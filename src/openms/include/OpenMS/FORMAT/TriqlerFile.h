08baecb3732ab0f57703cc8626cb9a26e1558a93
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2020-11-17 10:11:28 +0000

    Triqler support

--- src/openms/include/OpenMS/FORMAT/TriqlerFile.h
+++ src/openms/include/OpenMS/FORMAT/TriqlerFile.h
@@ -29,7 +29,7 @@
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Timo Sachsenberg $
-// $Authors: Timo Sachsenberg, Lukas Heumos $
+// $Authors: Timo Sachsenberg $
 // --------------------------------------------------------------------------
 
 #pragma once
@@ -50,39 +50,28 @@ namespace OpenMS
   using IndProtGrps = std::vector<IndProtGrp>;
 
   /**
-    @brief File adapter for MSstats files
+    @brief File adapter for Triqler files
     @ingroup FileIO
   */
   
-    class OPENMS_DLLAPI MSstatsFile
+    class OPENMS_DLLAPI TriqlerFile
     {
     public:
         /// Default constructor
-        MSstatsFile() = default;
+        TriqlerFile() = default;
         /// Destructor
-        ~MSstatsFile() = default;
+        ~TriqlerFile() = default;
 
-        /// store label free experiment (MSstats)
+        /// store label free experiment
         void storeLFQ(const String& filename, 
                       const ConsensusMap &consensus_map, // we might add singleton protein groups
                       const ExperimentalDesign& design,
                       const StringList& reannotate_filenames,
-                      const bool is_isotope_label_type,
-                      const String& bioreplicate,
                       const String& condition,
                       const String& retention_time_summarization_method);
         
-        /// store isobaric experiment (MSstatsTMT)
-        void storeISO(const String& filename, 
-                      const ConsensusMap &consensus_map,
-                      const ExperimentalDesign& design,
-                      const StringList& reannotate_filenames,
-                      const String& bioreplicate,
-                      const String& condition,
-                      const String& mixture,
-                      const String& retention_time_summarization_method);
-
     private:
+
       typedef OpenMS::Peak2D::IntensityType Intensity;
       typedef OpenMS::Peak2D::CoordinateType Coordinate;
 
@@ -109,21 +98,16 @@ namespace OpenMS
         *  such as filenames, intensities, retention times, labels and features.
         *  Stores them in AggregatedConsensusInfo for later processing
         */
-      MSstatsFile::AggregatedConsensusInfo aggregateInfo_(const ConsensusMap& consensus_map,
+      TriqlerFile::AggregatedConsensusInfo aggregateInfo_(const ConsensusMap& consensus_map,
                                                           const std::vector<String>& spectra_paths);
 
       /*
-        *  @brief: Internal function to check if MSstats_BioReplicate and MSstats_Condition exists in Experimental Design
+        *  @brief: Internal function to check if condition exists in Experimental Design
         */
-      static void checkConditionLFQ_(const ExperimentalDesign::SampleSection& sampleSection, const String& bioreplicate, const String& condition);
+      static void checkConditionLFQ_(const ExperimentalDesign::SampleSection& sampleSection, const String& condition);
 
       /*
-        *  @brief: Internal function to check if MSstats_BioReplicate, MSstats_Condition and MSstats_Mixture in Experimental Design
-        */
-      static void checkConditionISO_(const ExperimentalDesign::SampleSection& sampleSection, const String& bioreplicate, const String& condition, const String& mixture);
-
-      /*
-        *  @brief MSstats treats runs differently than OpenMS. In MSstats, runs are an enumeration of (SpectraFilePath, Fraction)
+        *  @brief Triqler treats runs differently than OpenMS. In Triqler, runs are an enumeration of (SpectraFilePath, Fraction)
         *  In OpenMS, a run is split into multiple fractions.
         */
       static void assembleRunMap_(
@@ -150,32 +134,25 @@ namespace OpenMS
         return sumIntensity_(intensities) / intensities.size();
       }
 
-      class MSstatsLine_
+      class TriqlerLine_
       {
       public :
-        MSstatsLine_(
-            bool _has_fraction,
-            const String& _accession,
-            const String& _sequence,
-            const String& _precursor_charge,
-            const String& _fragment_ion,
-            const String& _frag_charge,
-            const String& _isotope_label_type,
-            const String& _condition,
-            const String& _bioreplicate,
-            const String& _run,
-            const String& _fraction
-        ): has_fraction_(_has_fraction),
-            accession_(_accession),
-            sequence_(_sequence),
-            precursor_charge_(_precursor_charge),
-            fragment_ion_(_fragment_ion),
-            frag_charge_(_frag_charge),
-            isotope_label_type_(_isotope_label_type),
-            condition_(_condition),
-            bioreplicate_(_bioreplicate),
-            run_(_run),
-            fraction_(_fraction) {}
+        TriqlerLine_(
+            const String& run,
+            const String& condition,
+            const String& precursor_charge,
+            const String& search_score,           
+            const String& intensity,
+            const String& sequence,
+            const String& accession
+        ):  run_(run),
+            condition_(condition),
+            precursor_charge_(precursor_charge),
+            search_score_(search_score),
+            intensity_(intensity),
+            sequence_(sequence),
+            accession_(accession)
+             {}
 
         const String& accession() const {return this->accession_;}
         const String& sequence() const {return this->sequence_;}
@@ -184,105 +161,32 @@ namespace OpenMS
 
         String toString() const
         {
-          const String delim(",");
-          return  accession_
-                  + delim + sequence_
-                  + delim + precursor_charge_
-                  + delim + fragment_ion_
-                  + delim + frag_charge_
-                  + delim + isotope_label_type_
+          const String delim("\t");
+          return  run_
                   + delim + condition_
-                  + delim + bioreplicate_
-                  + delim + run_
-                  + (this->has_fraction_ ? delim + String(fraction_) : "");
+                  + delim + precursor_charge_
+                  + delim + search_score_
+                  + delim + intensity_
+                  + delim + sequence_
+                  + delim + accession_;
         }
 
-        friend bool operator<(const MSstatsLine_ &l,
-                              const MSstatsLine_ &r) {
+        friend bool operator<(const TriqlerLine_ &l,
+                              const TriqlerLine_ &r) {
 
-          return std::tie(l.accession_, l.run_, l.condition_, l.bioreplicate_, l.precursor_charge_, l.sequence_) <
-                  std::tie(r.accession_, r.run_, r.condition_, r.bioreplicate_, r.precursor_charge_, r.sequence_);
+          return std::tie(l.accession_, l.run_, l.condition_, l.precursor_charge_, l.intensity_, l.sequence_) <
+                  std::tie(r.accession_, r.run_, r.condition_, r.precursor_charge_, r.intensity_, r.sequence_);
         }
 
 
       private:
-        bool has_fraction_;
-        String accession_;
-        String sequence_;
-        String precursor_charge_;
-        String fragment_ion_;
-        String frag_charge_;
-        String isotope_label_type_;
-        String condition_;
-        String bioreplicate_;
         String run_;
-        String fraction_;
-      };
-
-      class MSstatsTMTLine_
-      {
-      public :
-        MSstatsTMTLine_(
-            const String& _accession,
-            const String& _sequence,
-            const String& _precursor_charge,
-            const String& _channel,
-            const String& _condition,
-            const String& _bioreplicate,
-            const String& _run,
-            const String& _mixture,
-            const String& _techrepmixture,
-            const String& _fraction
-        ): accession_(_accession),
-            sequence_(_sequence),
-            precursor_charge_(_precursor_charge),
-            channel_(_channel),
-            condition_(_condition),
-            bioreplicate_(_bioreplicate),
-            run_(_run),
-            mixture_(_mixture),
-            techrepmixture_(_techrepmixture),
-            fraction_(_fraction) {}
-
-        const String& accession() const {return this->accession_;}
-        const String& sequence() const {return this->sequence_;}
-        const String& precursor_charge() const {return this->precursor_charge_;}
-        const String& run() const {return this->run_;}
-
-        String toString() const
-        {
-          const String delim(",");
-          return  accession_
-                  + delim + sequence_
-                  + delim + precursor_charge_
-                  + delim + channel_
-                  + delim + condition_
-                  + delim + bioreplicate_
-                  + delim + run_
-                  + delim + mixture_
-                  + delim + techrepmixture_
-                  + delim + String(fraction_);
-        }
-
-        friend bool operator<(const MSstatsTMTLine_ &l,
-                              const MSstatsTMTLine_ &r) {
-
-          return std::tie(l.accession_, l.run_, l.condition_, l.bioreplicate_, l.mixture_, l.precursor_charge_, l.sequence_, l.channel_) <
-                  std::tie(r.accession_, r.run_, r.condition_, r.bioreplicate_, r.mixture_, r.precursor_charge_, r.sequence_, r.channel_);
-        }
-
-
-      private:
-        String accession_;
-        String sequence_;
-        String precursor_charge_;
-        String channel_;
         String condition_;
-        String bioreplicate_;
-        String run_;
-        String mixture_;
-        String techrepmixture_;
-        String fraction_;
+        String precursor_charge_;
+        String search_score_;
+        String intensity_;
+        String sequence_;
+        String accession_;
       };
 
       /*
